46ee55710a80f926f64dc6b1cfc29c891e6ffdb7
lancelot <LMao-DEVPRO@github.com>
2023-07-05 09:01:17 +0200

    ajout de la fonctionnalité d'édition manuelle du timecode in et out

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QMessageBox, QAction, QFileDialog, QPushButton, QLabel
+from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QMessageBox, QAction, QFileDialog, QPushButton, QLabel, QLineEdit
 from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtGui import QIcon
 from list_file_widget import List_file_widget
@@ -22,12 +22,14 @@ class GUI(QMainWindow):
         # Clip Gestion
         self.inButton   = QPushButton("In")
         self.inButton   .clicked.connect(self.setIn)
-        self.inLabel    = QLabel()
-        self.inLabel    .setText("00:00")
+        self.inLabel    = QLineEdit()
+        self.inLabel    .setText("0.000")
+        self.inLabel    .textEdited.connect(self.setInManual)
         self.outButton  = QPushButton("Out")
         self.outButton  .clicked.connect(self.setOut)
-        self.outLabel   = QLabel()
-        self.outLabel   .setText("00:00")
+        self.outLabel   = QLineEdit()
+        self.outLabel   .setText("0.000")
+        self.outLabel   .textEdited.connect(self.setOutManual)
         self.generate   = QPushButton("Generate")
         self.generate   .clicked.connect(self.generateCut)
 
@@ -49,11 +51,19 @@ class GUI(QMainWindow):
         print("IN")
         self.cutVideo.setTimeIn(self.videoReader.mediaPlayer.position()/1000)
         self.inLabel.setText(str(self.cutVideo.timein))
+    
+    def setInManual(self):
+        print("IN manual")
+        self.cutVideo.setTimeIn(float(self.inLabel.text()))
 
     def setOut(self):
         print("OUT")
         self.cutVideo.setTimeOut(self.videoReader.mediaPlayer.position()/1000)
         self.outLabel.setText(str(self.cutVideo.timeout))
+    
+    def setOutManual(self):
+        print("OUT manual")
+        self.cutVideo.setTimeOut(float(self.outLabel.text()))
 
     def generateCut(self):
         print("Generate")
